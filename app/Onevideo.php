1046af2a6b12836455038d74f37e08c59f59419a
barmaley4ik <my.ofice@gmail.com>
2018-10-23 13:09:36 +0300

    translate video

--- app/Onevideo.php
+++ app/Onevideo.php
@@ -3,9 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
 
 
 class Onevideo extends Model
 {
- 
+ protected $translatable = ['caption', 'caption_button'];
 }
