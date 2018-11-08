c63816bd6e7583a2c805ae624b5ee9e9c0411d1c
barmaley4ik <my.ofice@gmail.com>
2018-11-08 11:23:18 +0200

    Update resources/views/layouts/header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,20 +10,15 @@
 <!-- header
 ================================================== -->
 <header class="s-header">
-
     <div class="row">
         <div class="header-logo">
             <a class="site-logo" href="/"><img src="{{ Voyager::image($landing->thumbnail('cropped_logo', 'logo')) }}" title="{{$landing->title_image}}" alt="{{$landing->alt_image}}"></a>
         </div>
-
         <nav class="header-nav-wrap">
             {{ menu('landing', 'my_menu') }}
         </nav> <!-- end header-nav-wrap -->
-
         <a class="header-menu-toggle" href="#0">
             <span class="header-menu-icon"></span>
         </a>
-
     </div> <!-- end row -->
-
 </header> <!-- end s-header -->
